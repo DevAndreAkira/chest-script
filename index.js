3b8ff5c410dba4d61a66a608a89fd2f7af97c974
DevAndreAkira <pianista.akira@gmail.com>
2022-11-04 00:07:53 -0300

    fix: ajustando script2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.body.style.background = 'black';
 
-let arrayItem = document.querySelectorAll(".btn_script");
+let arrayItem = document.querySelectorAll("body > div.w-3\\/4.max-w-\\[370px\\].mx-auto > ul > li:nth-child(n)");
+let arrayBtn = document.querySelectorAll(".btn_script");
 let arrayTitle = document.querySelectorAll("ul > li:nth-child(n) > span");
-let arrayButton = document.querySelectorAll("ul > li:nth-child(n) > button");
 let arrayScript = [
   `var localCards = document.querySelectorAll("INSIRA_A_LISTA_AQUI");function cardsIguais(localCards) { let cardsIcons = localCards; let alturaCards = 0; cardsIcons.forEach((e, index) => { if (cardsIcons[index].offsetHeight > alturaCards) { alturaCards = cardsIcons[index].offsetHeight; }; }); cardsIcons.forEach((e, index) => { cardsIcons[index].style.height = alturaCards + "px"; })}cardsIguais(localCards);window.onresize = function () { let cardsIcons = localCards; cardsIcons.forEach((e, index) => { cardsIcons[index].style.height = "auto"; }); cardsIguais(localCards);}`,
   `var localCards = document.querySelectorAll("INSIRA_A_LISTA_AQUI");function cardsIguais(localCards) { let cardsIcons = localCards; let larguraCards = 0; cardsIcons.forEach((e, index) => { if (cardsIcons[index].offsetWidth > larguraCards) { larguraCards = cardsIcons[index].offsetWidth; }; }); cardsIcons.forEach((e, index) => { cardsIcons[index].style.width = larguraCards + "px"; })}cardsIguais(localCards);window.onresize = function () { let cardsIcons = localCards; cardsIcons.forEach((e, index) => { cardsIcons[index].style.width = "auto"; }); cardsIguais(localCards);}`,
@@ -10,17 +10,17 @@ let arrayScript = [
   `function limitadorChars(element, limiteNumero) { let osParagrafos = element; osParagrafos.forEach((e, index) => { let chars = osParagrafos[index]; let novaFrase = ''; chars = chars.textContent.split(""); chars.forEach((e, i) => { if (i < limiteNumero) { novaFrase = novaFrase + chars[i]; } else { } }) osParagrafos[index].textContent = novaFrase + (novaFrase.length >= limiteNumero ? "..." : ''); })}limitadorChars(INSIRA_A_LISTA_AQUI, INSIRA_NUMERO_MAXIMO);`
 ];
 
-//^ LIMITADOR DE CARACTERES
 arrayItem.forEach((e, i) => {
   e.onclick = function () {
     navigator.clipboard.writeText(arrayScript[i]);
-    arrayButton[i].textContent = 'check';
+    arrayBtn[i].textContent = 'check';
     setTimeout(() => {
-      arrayButton[i].textContent = 'content_copy';
+      arrayBtn[i].textContent = 'content_copy';
     }, 1000);
   }
 })
 
+//^ LIMITADOR DE CARACTERES
 function limitadorChars(element, limiteNumero) {
   let osParagrafos = element;
   osParagrafos.forEach((e, index) => {
